b2c0e95a322b5b6eb3cc1b6c40c01a4276291cc4
Thomas Nguyen <TNguyen@spartaglobal.com>
2019-12-16 11:58:58 +0000

    Added a way to load it and get the values within dictionary

--- json_basics.py
+++ json_basics.py
@@ -1,9 +1,9 @@
 import json
 
-
+# Defining the dictionary
 car_dictionary = {
     'name': 'tesla',
-    'engine': '90000kj',
+    'engine': '90000kw',
     'type': 'electric'
 }
 
@@ -13,11 +13,18 @@ print(car_dictionary)
 #Use this to create JSON string from our dictionaries on the go
 # json.dumps(dict_obj) --> string
 car_data_json_string = json.dumps(car_dictionary)
-print(type(car_data_json_string))
+print(type(car_data_json_string)) # Checking the different types dict and now should be string
 print(car_data_json_string)
 
 # json.dump(dict_object, file) --> writes json to a file
-# a new file will be created here including the string
+# The new file will be created 'new_json_file.json' (without me doing it) including the string
 with open('new_json_file.json', 'w') as jsonfile:
     json.dump(car_dictionary, jsonfile)
+
+#json.load(jsonfile) ----> dictionary
+# Loading it as a dictionary again to read it
+with open('new_json_file.json', 'r') as jsonfile:
+    car = json.load(jsonfile)
+    print(type(car))
+    print(car['engine']) # Getting value of car's engine in the dictionary
 
